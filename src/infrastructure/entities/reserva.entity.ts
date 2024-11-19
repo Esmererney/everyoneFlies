5aaf64281a6cdc9790388736a6b78e7db5aa8226
leydifranco40 <158230623+leydifranco40@users.noreply.github.com>
2024-11-19 11:26:32 -0500

    actualizacion de entities

--- src/infrastructure/entities/reserva.entity.ts
+++ src/infrastructure/entities/reserva.entity.ts
@@ -1,37 +1,27 @@
-import { Entity, Column, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, OneToOne, JoinColumn } from "typeorm";
+
 
 @Entity("reserva")
 export class ReservaEntity {
   
-  @PrimaryGeneratedColumn()
-  id_reserva!: number ; 
+  @PrimaryGeneratedColumn({ name: "id_reserva" })
+  id_reserva!: number;
 
-  @Column()
+  @Column({ name: "cod_vuelo", type: "varchar", length: 10 })
   cod_vuelo!: string;
 
-  @Column()
-  fecha_reserva: string;
+  @CreateDateColumn({ name: "fecha_reserva", type: "datetime" })
+  fecha_reserva?: Date;
 
-  @Column()
-  estado_reserva: string;
+  @Column({ name: "estado_reserva", type: "varchar", length: 100 })
+  estado_reserva?: string;
 
-  @Column()
-  cantidad_pasajeros: number;
+  @Column({ name: "cantidad_pasajeros", type: "int" })
+  cantidad_pasajeros?: number;
 
-  constructor(body:{
-    id_reserva:number
-    cod_vuelo:string
-    fecha_reserva: string
-    estado_reserva:string
-    cantidad_pasajeros:number
-  }) {
-    this.id_reserva = body?.id_reserva;
-    this.cod_vuelo= body?.cod_vuelo
-    this.fecha_reserva =body?.fecha_reserva
-    this.estado_reserva = body?.estado_reserva
-    this.cantidad_pasajeros = body?.cantidad_pasajeros
-  }
+  @OneToOne(() => Vuelo, (vuelo) => vuelo.reserva)
+  @JoinColumn({ name: "cod_vuelo" })
+  vuelo: Vuelo;
+}
 
   
-
-}
--- src/infrastructure/entities/tickets.entity.ts
+++ src/infrastructure/entities/tickets.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, Unique, ManyToOne, OneToOne, JoinColumn,  CreateDateColumn } from "typeorm";
+import { ReservaEntity} from "./reserva.entity";
 
 @Entity("tickets")
 export class TicketsEntity {
@@ -6,39 +7,21 @@ export class TicketsEntity {
   @PrimaryGeneratedColumn()
   id_ticket!: number ; 
 
-  @Column()
-  id_reserva!: number;
+  @Column({ name: "estado_ticket", type: "enum", enum: ["emitido", "check-in", "abordado", "finalizado", "cancelado"] })
+  estado_ticket?: string;
 
-  @Column()
-  cod_vuelo: string;
+  @CreateDateColumn({ name: "fecha_emision", type: "datetime" })
+  fecha_emision?: Date;
 
-  @Column()
-  id_pasajero: number;
+  @OneToOne(() => ReservaEntity, (reserva) => reserva.id_reserva)
+  @JoinColumn({ name: "id_reserva" })
+  reserva?: ReservaEntity;
 
-  @Column()
-  fecha_emision: number;
-  
-  @Column()
-  estado_ticket: string
+  @OneToOne(() => Vuelo, (vuelo) => vuelo.cod_vuelo)
+  @JoinColumn({ name: "cod_vuelo" })
+  vuelo: Vuelo;
 
-  constructor(body:{
-    id_ticket: number
-    id_reserva:number
-    cod_vuelo:string
-    id_pasajero:number
-    fecha_emision: number
-    estado_ticket: string
-    
-   
-  }) {
-    this.id_ticket = body?.id_ticket
-    this.id_reserva = body?.id_reserva;
-    this.cod_vuelo= body?.cod_vuelo
-    this.id_pasajero = body?.id_pasajero
-    this.fecha_emision = body?.fecha_emision
-    this.estado_ticket = body?.estado_ticket
-  }
-
-  
-
+  @OneToOne(() => Pasajero, (pasajero) => pasajero.id_pasajero,)
+  @JoinColumn({ name: "id_pasajero" })
+  pasajero: Pasajero;
 }
