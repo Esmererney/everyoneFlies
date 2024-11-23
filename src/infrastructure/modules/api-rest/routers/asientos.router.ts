8fa2ac2ff18cc3d7cfd4e83c1591c7f80550bd93
jorge101280 <jorge-alejandro-101@hotmail.com>
2024-11-23 18:00:19 -0500

    swagger para asiento routes
    swagger para asiento routes

--- src/infrastructure/modules/api-rest/routers/asientos.router.ts
+++ src/infrastructure/modules/api-rest/routers/asientos.router.ts
@@ -7,6 +7,45 @@ export const RutasAsiento = () => {
   const asientoCtrl = new AsientoController();
 
   // Ruta para obtener todos los asientos
+
+  // swagger:
+  
+  /** 
+   * @swagger
+   * /asientos:
+   *   get: 
+   *     description: Obtiene todos los asientos
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Asientos obtenidos
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 type: object
+   *               properties:
+   *                 id_asiento: 
+   *                   type: number
+   *                   description: ID del asiento
+   *                 cod_vuelo: 
+   *                   type: string
+   *                   description: Código del vuelo al que pertenece el asiento
+   *                 disponible: 
+   *                   type: boolean
+   *                   description: Indica si el asiento está disponible o no
+   *                 id_categoria: 
+   *                   type: number
+   *                   description: ID de la categoría al que pertenece el asiento
+   *       500:
+   *         description: Error al obtener los asientos
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   */ 
   router.get("/asientos", (req, res) => { 
     asientoCtrl.obtener().then((result) => {
       res.send(result);
@@ -16,7 +55,40 @@ export const RutasAsiento = () => {
     });
   });
 
+
+
   // Ruta para agregar un nuevo asiento
+
+  // swagger:
+  /** 
+   * @swagger
+   * /asientos:
+   *   post:
+   *     description: Agrega un nuevo asiento
+   *     tags:
+   *       - Asientos
+   *     requestBody:
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object 
+   *             properties:
+   *               cod_vuelo:
+   *                 type: string
+   *                 description: Código del vuelo al que pertenece el asiento
+   *               disponible:
+   *                 type: boolean
+   *                 description: Indica si el asiento está disponible o no
+   *               id_categoria:
+   *                 type: number
+   *                 description: ID de la categoría al que pertenece el asiento
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       201:
+   *         description: Asiento creado
+   *   
+   */
   router.post("/asientos", (req, res) => { 
     const payload = req.body;
     asientoCtrl.agregar(payload).then((result) => {
@@ -28,6 +100,44 @@ export const RutasAsiento = () => {
   });
 
   // Ruta para actualizar un asiento
+
+  //swagger 
+  /** 
+   * @swagger
+   * /asientos/{id}:
+   *   put:
+   *     description: Actualiza un asiento
+   *     tags:
+   *       - Asientos
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         description: ID del asiento a actualizar
+   *         required: true
+   *         schema:
+   *           type: number
+   *     requestBody:
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object 
+   *             properties:
+   *               cod_vuelo:
+   *                 type: string
+   *                 description: Código del vuelo al que pertenece el asiento
+   *               disponible:
+   *                 type: boolean
+   *                 description: Indica si el asiento está disponible o no
+   *               id_categoria:
+   *                 type: number
+   *                 description: ID de la categoría al que pertenece el asiento
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Asiento actualizado
+   *   
+   */ 
   router.put("/asientos", (req, res) => {  
     const payload = req.body;
     asientoCtrl.actualizar(payload).then((result) => {
@@ -39,6 +149,32 @@ export const RutasAsiento = () => {
   });
 
   // Ruta para obtener un asiento por su ID
+
+  //swagger 
+  /** 
+   * @swagger
+   * /asientos/{id}:
+   *   get:
+   *     description: Obtiene un asiento por su ID
+   *     tags:
+   *       - Asientos
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         description: ID del asiento a obtener
+   *         required: true
+   *         schema:
+   *           type: number
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Asiento obtenido
+   *       404:
+   *         description: Asiento no encontrado
+   *   
+   */
+  
   router.get("/asientos/:id", async (req, res) => { 
     try {
       const idStr = req.params.id;
@@ -58,7 +194,34 @@ export const RutasAsiento = () => {
     }
   });
 
+
+
   // Ruta para eliminar un asiento por su ID
+
+  //swagger 
+  /** 
+   * @swagger
+   * /asientos/{id}:
+   *   delete:
+   *     description: Elimina un asiento por su ID
+   *     tags:
+   *       - Asientos
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         description: ID del asiento a eliminar
+   *         required: true
+   *         schema:
+   *           type: number
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Asiento eliminado
+   *       400:
+   *         description: Error al eliminar el asiento
+   *   
+   */ 
   router.delete("/asientos/:id", async (req, res) => {
     try {
       const idStr = req.params.id;
